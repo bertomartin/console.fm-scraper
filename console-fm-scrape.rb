f9e4b9b5898344637247b5dfcc9a88e27a811a06
Nick Donohue <ndonohue@gmail.com>
2011-09-11 22:54:53 -0400

    optparser and cleanup
    uses the optionparser class now. Also updated all "config" variables to
    use the class.  Verbose is useless right now, and list is not where'd
    I'd like it to be yet, but it does list all the genres.

--- console-fm-scrape.rb
+++ console-fm-scrape.rb
@@ -9,97 +9,121 @@
 #####################################
 
 require "open-uri"
+require 'optparse'
+
+
+# _ _ ._  _ _ | _ _|_._ _  (_ / |_) /\\ |_)|_
+#(_(_)| |_>(_)|(/_o| | | | __)\\_| \\/--\\|  |_
+#
+#usage: ruby console-fm-scrape.rb <genre>
+
+categories = [
+	"Top (default)",
+	"Breaks",
+	"Chill-out",
+	"Deep-house",
+	"Drum-and-bass",
+	"Dubstep",
+	"Electro-house",
+	"Electronica",
+	"Funk-r-and-b",
+	"Glitch-Hop",
+	"Hard-dance",
+	"Hardcore-hard-techno",
+	"Hip-hop",
+	"House",
+	"Indie-dance-nu-disco",
+	"Minimal",
+	"Pop-rock",
+	"Progressive-house",
+	"Psy-trance",
+	"Reggae-dub",
+	"Tech-house",
+	"Techno",
+	"Trance"
+]
+
 
 ######### TINY BIT OF CONFIG #########
-dir = "./music-downloads" # default save dir
-######################################
+options = {}
+options [:verbose] = false
+options [:category] = "top"
+options [:directory] = "./music-downloads"
+options [:list] = false
+
+OptionParser.new do |opts|
+	opts.banner = "Usage: %s [options]" % $0
+
+	opts.on("-v", "--verbose", "Run verbosely") do 
+	  options[:verbose] = true
+	end
+
+	opts.on("-c", "--category [CATEGORY]", "use [category]. Default is top") do |c|
+	  options[:category] = c
+	end
+
+	opts.on("-d", "--directory [DIRECTORY]", "download to [directory]") do |d|
+	  options[:directory] = d
+	end
+ 
+	opts.on("-l", "--list [CATEGORY]", "list songs before downloading them. Lists categories if no argument is supplied") do |l|
+	  options[:list] = l || true
+	end
+
+end.parse!
 
 
-puts """
-                   _       __ _ _      _  _
- _ _ ._  _ _ | _ _|_._ _  (_ / |_) /\\ |_)|_
-(_(_)| |_>(_)|(/_o| | | | __)\\_| \\/--\\|  |_
+if options[:verbose] == true then
+	puts "verbose"
+end
 
-usage: ruby console-fm-scrape.rb <genre>
-
-"""
-
-if not File.directory?(dir) then
-	puts "[!] download directory #{dir} does not exist!"
+# checks for existence of download directory
+if not File.directory?(options[:directory]) then
+	puts "[!] download directory #{options[:directory]} does not exist!"
 	puts "[!] please create it before running!"
 	Kernel.exit
 end
 
-category = "top"
-
-category = ARGV[0] if ARGV.size > 0
-
-if category == "-h" or category == "--help" then
-	puts """
-
-usage: ruby console-fm-scrape.rb <genre>
-If no genre is specified, the 'top' playlist is scraped.
-
-Current available playlists include:
-Top (default)
-Breaks
-Chill-out
-Deep-house
-Drum-and-bass
-Dubstep
-Electro-house
-Electronica
-Funk-r-and-b
-Glitch-Hop
-Hard-dance
-Hardcore-hard-techno
-Hip-hop
-House
-Indie-dance-nu-disco
-Minimal
-Pop-rock
-Progressive-house
-Psy-trance
-Reggae-dub
-Tech-house
-Techno
-Trance
-
-"""
-
-Kernel.exit
-
+# print list of categories if user wants to
+if options[:list] == true then
+	puts "Current available playlists include:"
+	categories.each do |category|
+		puts category
+	end
+	Kernel.exit
 end
 
-target = "http://console.fm/#{category}"
+target = "http://console.fm/#{options[:category]}"
 puts "[+] trying to reach #{target}"
 
 open(target) do |p|
 	p.each_line do |line|
 		if line =~ /http:\/\/media.console.fm\/tracks\/\d+/
-      			location = $&
-      			line =~ /\">(.*)\</
-      			name = $1[0..-5]
+			location = $&
+			line =~ /\">(.*)\</
+			name = $1[0..-5]
 			name.gsub!(/\//, '_')
-      			puts "[+] grabbing track #{name}"
+			fname = "#{options[:directory]}/#{name}.mp3"
 
-      			if not File.exists?("#{dir}/#{name}.mp3")
-      				f = File.new("#{dir}/#{name}.mp3", "wb")
-				begin
-      					f.write(open(location, "Cookie" => p.meta['set-cookie']).read)
-				rescue
-					puts "[!] ERROR -- deleting empty file #{name}.mp3"
-					File.delete("#{dir}/#{name}.mp3")
-					next
-				end
-      				f.close
-				sleep (rand(10) +5) # sleep 10-20 seconds
-      			else
-				puts "[!] detected duplicate file #{name}.mp3, skipping"
-      			end
-    		end
+			print "[+] grabbing track #{name}..."
+
+			if not File.exists?(fname)
+				f = File.new(fname, "wb")
+			begin
+				f.write(open(location, "Cookie" => p.meta['set-cookie']).read)
+			rescue
+				print "ERROR \n"
+				File.delete(fname)
+				next
+			end
+				f.close
+			print "done \n"
+			sleep (5) # sleep 10-20 seconds
+			else
+				print "exists \n"
+			end
+		end
   	end
 end
 
 puts "[+] script completed"
-puts "[+] if you like these songs, support the artists and console.fm!"
